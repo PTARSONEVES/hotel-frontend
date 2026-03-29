ed71f1ed6f0b6066a0f335af23a8735ddc799a6b
Paulo de Tarso Neves de Araújo <50182156+PTARSONEVES@users.noreply.github.com>
2026-03-29 13:38:14 -0300

    Atualização do local de ThemeContext

--- src/landing/components/Footer.jsx
+++ src/landing/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useTheme } from '../../../context/ThemeContext';
+import { useTheme } from '../../context/ThemeContext';
 
 export default function Footer() {
     const { theme } = useTheme();
